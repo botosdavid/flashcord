e10f823e0623b13e50607faa1aa2da6dd0c3ce42
David <davidbotos60@yahoo.ro>
2022-01-07 14:47:37 +0200

    Server changes

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
-const socket = io('http://localhost:3000', {transports: ['websocket']});
+// const socket = io('http://localhost:3000', {transports: ['websocket']});
+const socket = io('https://mybrary-bda.flashcord.com',{transports: ['websocket']})
 const roomButton = document.querySelector('#room-button');
 const roomInput = document.querySelector('#room-input');
 const roomContainer = document.querySelector('#rooms-container');
